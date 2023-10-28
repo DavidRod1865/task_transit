c2c0b44db74492d567961308454013192d996b74
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-27 23:47:36 -0400

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
+import LoginButton from "./components/LoginButton";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<LoginButton />} />
         </Routes>
     );
 }
