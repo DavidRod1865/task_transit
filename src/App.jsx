34ce2372d6dc81f47b7db43146c6c93b520cdc3b
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-27 23:34:58 -0400

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Login from "./components/Login";
 
 function App() {
     return (
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
     );
-  }
-export default App
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'
-import './index.css'
-import App from './App'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-  domain={'dev-sepjltc5sis0ivcn.us.auth0.com'}
-  clientId={'ogODyc9E2zVu7HM0v5d7XXKULOdWpL4n'}
-  authorizationParams={{
-    redirect_uri: window.location.origin,
-  }}
+    domain='dev-sepjltc5sis0ivcn.us.auth0.com'
+    clientId='ogODyc9E2zVu7HM0v5d7XXKULOdWpL4n'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://task-transit.vercel.app/api/v2/",
+      scope: "read:current_user update:current_user_metadata"
+    }}
   >
     <React.StrictMode>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </React.StrictMode>
   </Auth0Provider>,
-)
+);
