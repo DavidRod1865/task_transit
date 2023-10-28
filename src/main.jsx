5d33900685f226fb1b773eb9301ee10e7c3e2e38
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-27 23:13:36 -0400

    Added Auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
-import Login from './components/Login.jsx';
+import Login from './components/LoginButton.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
   domain={'dev-sepjltc5sis0ivcn.us.auth0.com'}
   clientId={'ogODyc9E2zVu7HM0v5d7XXKULOdWpL4n'}
   authorizationParams={{
-    redirect_uri: window.location.origin
+    redirect_uri: "https://task-transit.vercel.app/"
   }}
   >
     <React.StrictMode>
-      <RouterProvider router={router} />
+        <Login />
     </React.StrictMode>
   </Auth0Provider>,
 )
