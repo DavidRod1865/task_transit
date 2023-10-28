b47d562138c6007bd07ff79bc028ce732ab873e9
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-27 23:43:30 -0400

    Added Auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import LoginButton from './LoginButton';
 
-class Login {
-
-      render() {
+class Login extends React.Component {
+    render() {
         return <LoginButton />;
-      }
     }
-    
-export default Login;
+}
 
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
